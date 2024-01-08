ac14637223370471a70229ed53177fa5e98178a9
Silensoor <Vitalick78084@gmail.com>
2024-01-08 18:09:58 +0300

    hw15-pattern (add memento)

--- hw15-pattern/homework/src/main/java/ru/otus/processor/homework/ProcessorThrowExceptionOnEvenSecond.java
+++ hw15-pattern/homework/src/main/java/ru/otus/processor/homework/ProcessorThrowExceptionOnEvenSecond.java
@@ -3,13 +3,23 @@ package ru.otus.processor.homework;
 import ru.otus.model.Message;
 import ru.otus.processor.Processor;
 
+import java.time.LocalTime;
+
 public class ProcessorThrowExceptionOnEvenSecond implements Processor {
 
   @Override
   public Message process(Message message) {
-    if (System.currentTimeMillis() % 2 == 0) {
-      throw new RuntimeException("Even second exception");
+    int seconds = LocalTime.now().getSecond();
+    if (seconds % 2 == 0) {
+      RuntimeException lastException = new RuntimeException("Even second exception");
+      saveState(seconds, lastException);
+      throw lastException;
     }
     return message;
+  }
+
+  public void saveState(long lastProcessedTime, RuntimeException lastException) {
+    ProcessorMemento processorMemento = new ProcessorMemento(lastProcessedTime, lastException);
+    LogMemento.addLog(processorMemento);
   }
 }
--- hw15-pattern/homework/src/test/java/ru/otus/listener/homework/HistoryListenerTest.java
+++ hw15-pattern/homework/src/test/java/ru/otus/listener/homework/HistoryListenerTest.java
@@ -1,40 +1,77 @@
 package ru.otus.listener.homework;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
+import java.time.LocalTime;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import ru.otus.handler.ComplexProcessor;
 import ru.otus.model.Message;
 import ru.otus.model.ObjectForMessage;
+import ru.otus.processor.Processor;
+import ru.otus.processor.homework.LogMemento;
+import ru.otus.processor.homework.ProcessorMemento;
+import ru.otus.processor.homework.ProcessorThrowExceptionOnEvenSecond;
 
 class HistoryListenerTest {
 
-    @Test
-    void listenerTest() {
-        // given
-        var historyListener = new HistoryListener();
+  @Test
+  void listenerTest() {
+    // given
+    var historyListener = new HistoryListener();
 
-        var id = 100L;
-        var data = "33";
-        var field13 = new ObjectForMessage();
-        var field13Data = new ArrayList<String>();
-        field13Data.add(data);
-        field13.setData(field13Data);
+    var id = 100L;
+    var data = "33";
+    var field13 = new ObjectForMessage();
+    var field13Data = new ArrayList<String>();
+    field13Data.add(data);
+    field13.setData(field13Data);
 
-        var message = new Message.Builder(id)
-                .field10("field10")
-                .field13(field13)
-                .build();
+    var message = new Message.Builder(id)
+      .field10("field10")
+      .field13(field13)
+      .build();
 
-        // when
-        historyListener.onUpdated(message);
-        //message.getField13().setData(new ArrayList<>()); //меняем исходное сообщение
-        //field13Data.clear(); //меняем исходный список
+    // when
+    historyListener.onUpdated(message);
+    //message.getField13().setData(new ArrayList<>()); //меняем исходное сообщение
+    //field13Data.clear(); //меняем исходный список
 
-        // then
-        var messageFromHistory = historyListener.findMessageById(id);
-        assertThat(messageFromHistory).isPresent();
-        assertThat(messageFromHistory.get().getField13().getData()).containsExactly(data);
+    // then
+    var messageFromHistory = historyListener.findMessageById(id);
+    assertThat(messageFromHistory).isPresent();
+    assertThat(messageFromHistory.get().getField13().getData()).containsExactly(data);
+  }
+
+  @Test
+  void complexProcessorTest() throws InterruptedException {
+    List<Processor> processors = List.of(new ProcessorThrowExceptionOnEvenSecond());
+    var complexProcessor = new ComplexProcessor(processors, ex -> {
+    });
+
+    var message = new Message.Builder(1L)
+      .field1("field1")
+      .field2("field2")
+      .field3("field3")
+      .field6("field6")
+      .field10("field10")
+      .build();
+
+    complexProcessor.handle(message);
+
+    int seconds = LocalTime.now().getSecond();
+
+    if (LogMemento.getMementos().isEmpty()) {
+      assertThat(seconds % 2).isNotEqualTo(0);
+    } else {
+      ArrayDeque<ProcessorMemento> mementos = LogMemento.getMementos();
+      ProcessorMemento first = mementos.getFirst();
+      assertThat(first.lastProcessedTime() % 2).isEqualTo(0);
     }
+  }
 }
