93c84cd8cb51821ea4271127044b289f3c5db44d
Silensoor <Vitalick78084@gmail.com>
2024-02-12 21:44:21 +0300

    hw21-cache

--- hw16-io/homework/src/main/java/ru/otus/dataprocessor/Loader.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.dataprocessor;
-
-import java.util.List;
-import ru.otus.model.Measurement;
-
-public interface Loader {
-
-    List<Measurement> load();
-}
--- hw18-jdbc/homework/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-dependencies {
-    implementation(project(":hw18-jdbc:demo"))
-
-    implementation("ch.qos.logback:logback-classic")
-    implementation("org.flywaydb:flyway-core")
-    implementation("org.postgresql:postgresql")
-}
